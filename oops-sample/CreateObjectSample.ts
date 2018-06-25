2da92d5263d5016342a0447ec39b3566b6df8205
premprakashrohan <premprakashrohan@gmail.com>
2018-06-25 13:19:51 +0530

    declaration of prop in interface

--- oops-sample/CreateObjectSample.ts
+++ oops-sample/CreateObjectSample.ts
@@ -1,11 +1,22 @@
 interface IUser {
-    username: string;
-    password: string;
     // optional prop declaration
     email?: string;
+    password: string;
+    username: string;
+   // Dynamic property /undefined property
+    [prop: string]: any;
 }
 
 let admin: IUser = {
+    email: "premprakashrohan@gmail.com",
     password: "1234",
     username: "prem",
+    // below is caused by [prop: string]: any;
+    v: "Hello",
+    v1: "xyz",
+    v2: "xyz",
+    v3: "xyz",
+    v4: "xyz",
+    v5: "xyz",
+    v6: "xyz",
 };
