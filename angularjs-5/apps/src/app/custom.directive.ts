d6a46020941a4da9adacff1b0a54f646d971187c
premprakashrohan <premprakashrohan@gmail.com>
2018-06-28 00:17:21 +0530

    custome directives

--- angularjs-5/apps/src/app/custom.directive.ts
+++ angularjs-5/apps/src/app/custom.directive.ts
@@ -1,14 +1,27 @@
-import { Directive, ElementRef, Input, Renderer2, OnInit } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appCustom]'
 })
-export class CustomDirective implements OnInit {
-  @Input() appCustom;
+export class CustomDirective {
+  private range: number[];
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
-  ngOnInit() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', `${this.appCustom}`);
+  constructor(private viewContainerRef: ViewContainerRef, private templateRef: TemplateRef<any>) {
+
+
+  }
+
+  @Input() set appCustom(value) {
+    this.viewContainerRef.clear();
+    this.range = this.generteRange(value[0], value[1]);
+    console.log(this.range);
+    console.log('Working...');
+    this.range.map(number => this.viewContainerRef.createEmbeddedView(this.templateRef, { $implicit: number }));
+  }
+  // 10-15 -> [10, 11, 12, 13, 14, 15]
+  private generteRange(from, to): number[] {
+    console.log(to + '   ' + from);
+    return Array(to - from + 1 ).fill(0).map((_, index) => from + index );
   }
 
 }
