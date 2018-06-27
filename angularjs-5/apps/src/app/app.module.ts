ffc4abfe870fff853f97cf17254ec78f0dcea99a
premprakashrohan <premprakashrohan@gmail.com>
2018-06-27 16:43:41 +0530

    component by cli command

--- angularjs-5/apps/src/app/app.module.ts
+++ angularjs-5/apps/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first-component/first-component';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule
