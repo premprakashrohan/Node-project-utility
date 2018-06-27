18be40cb3bb023193f0779cecd553d13ff51bce1
premprakashrohan <premprakashrohan@gmail.com>
2018-06-27 20:40:39 +0530

    User angular Form

--- angularjs-5/apps/src/app/second/second.component.ts
+++ angularjs-5/apps/src/app/second/second.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
   styleUrls: ['./second.component.css']
 })
 export class SecondComponent implements OnInit {
-  product = { };
-  constructor() { }
+  form: FormGroup;
+  name = new FormControl('', Validators.required);
+  price = ['', Validators.required];
+  constructor(private formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      name: this.name,
+      price: this.price
+    });
+   }
 
   ngOnInit() {
   }
 
   submitForm() {
-    console.log(this.product);
+    console.log(this.form.value);
   }
 
 }
