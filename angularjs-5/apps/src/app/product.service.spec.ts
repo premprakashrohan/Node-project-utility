834f4a42e046f2613888a4b10480e49afa4216bf
premprakashrohan <premprakashrohan@gmail.com>
2018-06-28 20:54:14 +0530

    test

--- angularjs-5/apps/src/app/product.service.spec.ts
+++ angularjs-5/apps/src/app/product.service.spec.ts
@@ -1,15 +1,46 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, getTestBed, async } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
 
 import { ProductService } from './product.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ProductService', () => {
+  let injector: TestBed;
+  let service: ProductService;
+  let httpMock: HttpTestingController;
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule, HttpClientTestingModule],
       providers: [ProductService]
     });
+    injector = getTestBed();
+    service = injector.get(ProductService);
+    httpMock = injector.get(HttpTestingController);
   });
 
-  it('should be created', inject([ProductService], (service: ProductService) => {
-    expect(service).toBeTruthy();
-  }));
+  afterEach(() => httpMock.verify());
+
+  it('should issue a request', async(inject([HttpClient, HttpTestingController], (http: HttpClient, backend: HttpTestingController ) => {
+    http.get('http://localhost:3000/api/products').subscribe();
+    const request = httpMock.expectOne('http://localhost:3000/api/products');
+    expect(request.request.method).toBe('Get');
+  })));
+
+  it('should return products', () => {
+    const dummyProducts = [
+      {productName: 'Test Product', price: 1.99},
+      {productName: 'Another Product', price: 15.99}
+    ];
+
+    service.listProducts().subscribe((products: any) => {
+      expect(products.legth).toBe(2);
+      expect(products).toEqual(dummyProducts);
+
+    });
+    const request = httpMock.expectOne('http://localhost:3000/api/products');
+    expect( request.request.method).toBe('Get');
+    request.flush(dummyProducts);
+  });
+
 });
